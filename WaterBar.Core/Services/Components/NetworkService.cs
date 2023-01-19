0b041ccc7f1d3c8ca9be37368b5c22b74ac30a82
Gaein nidb <mail@gaein.cn>
2023-01-20 00:37:18 +0800

    fix:wrong time format in config file

--- WaterBar.Core/Services/Components/NetworkService.cs
+++ WaterBar.Core/Services/Components/NetworkService.cs
@@ -5,12 +5,13 @@ namespace WaterBar.Core.Services.Components;
 
 public class NetworkService : IComponentService
 {
+    private record ReceiveAndSend(double receive, double send);
+
     private readonly StatusBarOptionItem _optionItem;
     private readonly NetworkStatus _status;
 
     private DateTime _lastTime;
-    private ulong _lastReceivedBytes;
-    private ulong _lastSentBytes;
+    private ReceiveAndSend _lastReceiveAndSend;
 
     public NetworkService(StatusBarOptionItem optionItem)
     {
@@ -22,16 +23,16 @@ public class NetworkService : IComponentService
         var now = DateTime.Now;
         var interval = now - _lastTime;
 
-        var (upSpeed, downSpeed) = (
-            (_status.SentBytes - _lastSentBytes) / (1024 * interval.TotalSeconds),
-            (_status.ReceivedBytes - _lastReceivedBytes) / (1024 * interval.TotalSeconds)
+        var speed = new ReceiveAndSend(
+            (_status.SentBytes - _lastReceiveAndSend.receive) / (1024 * interval.TotalSeconds),
+            (_status.ReceivedBytes - _lastReceiveAndSend.send) / (1024 * interval.TotalSeconds)
         );
         var (upStr, downStr) = (
-            upSpeed > 1024 ? $"{upSpeed / 1024:F2}mb/s" : $"{upSpeed:F2}kb/s",
-            downSpeed > 1024 ? $"{downSpeed / 1024:F2}mb/s" : $"{downSpeed:F2}kb/s"
+            speed.send > 1024 ? $"{speed.send / 1024:F2}mb/s" : $"{speed.send:F2}kb/s",
+            speed.receive > 1024 ? $"{speed.receive / 1024:F2}mb/s" : $"{speed.receive:F2}kb/s"
         );
 
-        (_lastSentBytes, _lastReceivedBytes, _lastTime) = (_status.SentBytes, _status.ReceivedBytes, now);
+        (_lastReceiveAndSend, _lastTime) = (new(_status.SentBytes, _status.ReceivedBytes), now);
 
         return _optionItem.Format
             .Replace("up_speed", upStr)
