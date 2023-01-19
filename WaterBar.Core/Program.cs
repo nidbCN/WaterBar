194f1ed1016d9b916b19a1b46d2e3ad0bc1ab450
Gaein nidb <mail@gaein.cn>
2023-01-19 21:55:18 +0800

    feat:Init project

---
+++ WaterBar.Core/Program.cs
@@ -0,0 +1,27 @@
+using WaterBar.Core.Options;
+using WaterBar.Core.Services;
+using WaterBar.Core.Services.Components;
+
+#nullable disable
+IConfiguration configuration = null;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config => { configuration = config.Build(); })
+    .ConfigureServices(services =>
+    {
+        services.AddLogging();
+        services.Configure<StatusBarOption>(
+            configuration.GetSection(nameof(StatusBarOption))
+        );
+
+        services.AddSingleton<IStatusService, StatusService>();
+        services.AddSingleton<FactoryService>();
+    })
+    .Build();
+
+var statusService = host.Services.GetRequiredService<IStatusService>();
+
+statusService.StartOutput();
+
+host.Run();
+#nullable restore
